79d737fd6849aa48ce47c044adeaf2d9b98e19a4
FunnelKakeXYZ <developers@digidev.io>
2019-12-05 20:58:26 +0000

    init

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController{
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function homepage(){
+		return $this->render('homepage.html.twig');
+	}
+}
